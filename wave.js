07a0ad05862cb417837310109dcef8ee7c395593
suyasuyazzang <suyasuyazzang@gmail.com>
2021-07-20 16:17:53 +0900

    point & wave testing

---
+++ wave.js
@@ -0,0 +1,39 @@
+import {
+  Point
+} from './point.js'
+
+export class Wave{
+  constructor(){
+    
+  }
+
+  resize(stageWidth, stageHeight){
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
+
+    this.centerX = stageWidth / 2;
+    this.centerY = (stageHeight * 3) / 4
+
+    this.init();
+  }
+
+  init(){
+    this.point = new Point(
+      this.centerX,
+      this.centerY
+    );
+  }
+
+  draw(ctx){
+    ctx.beginPath();
+    ctx.fillStyle = '#ed9121';
+
+    this.point.update();
+
+    ctx.arc(
+      this.point.x, this.point.y,
+      30, 0, 2 * Math.PI
+    );
+    ctx.fill();
+  }
+}
